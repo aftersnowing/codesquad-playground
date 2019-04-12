2329e9efe7b1c016af00cdcb1c45985989e70680
aftersnowing <gurcks2@gmail.com>
2019-04-12 14:34:05 +0900

    json데이터 name출력하기 recursion과 closure를 이용하여 구현

--- 3-1 array&object/3-1 array & object.js
+++ 3-1 array&object/3-1 array & object.js
@@ -25,11 +25,11 @@ const data = {
     }
 }
 function pickNumData(obj) {
-    var result = [];
+    let result = [];
     (function closure(obj) {
-        var keysArr = Object.keys(obj);
+        let keysArr = Object.keys(obj);
         for (key of keysArr) {
-            var val = obj[key];
+            let val = obj[key];
             (function numFinder(val) {
                 if (typeof val === 'object') {
                     return closure(val);
@@ -45,3 +45,102 @@ function pickNumData(obj) {
 //숫자타입으로만 구성된 요소를 뽑아 배열만들기
 //실행결과
 // ["width", "height", "hOffset", "vOffset", "size", "hOffset", "vOffset"]
+
+
+
+var jsonData = [{
+	"id": 1,
+	"name": "Yong",
+	"phone": "010-0000-0000",
+	"type": "sk",
+	"childnode": [{
+		"id": 11,
+		"name": "echo",
+		"phone": "010-0000-1111",
+		"type": "kt",
+		"childnode": [{
+				"id": 115,
+				"name": "hary",
+				"phone": "211-1111-0000",
+				"type": "sk",
+				"childnode": [{
+					"id": 1159,
+					"name": "pobi",
+					"phone": "010-444-000",
+					"type": "kt",
+					"childnode": [{
+							"id": 11592,
+							"name": "cherry",
+							"phone": "111-222-0000",
+							"type": "lg",
+							"childnode": []
+						},
+						{
+							"id": 11595,
+							"name": "solvin",
+							"phone": "010-000-3333",
+							"type": "sk",
+							"childnode": []
+						}
+					]
+				}]
+			},
+			{
+				"id": 116,
+				"name": "kim",
+				"phone": "444-111-0200",
+				"type": "kt",
+				"childnode": [{
+					"id": 1168,
+					"name": "hani",
+					"phone": "010-222-0000",
+					"type": "sk",
+					"childnode": [{
+						"id": 11689,
+						"name": "ho",
+						"phone": "010-000-0000",
+						"type": "kt",
+						"childnode": [{
+								"id": 116890,
+								"name": "wonsuk",
+								"phone": "010-000-0000",
+								"type": "kt",
+								"childnode": []
+							},
+							{
+								"id": 1168901,
+								"name": "chulsu",
+								"phone": "010-0000-0000",
+								"type": "sk",
+								"childnode": []
+							}
+						]
+					}]
+				}]
+			},
+			{
+				"id": 117,
+				"name": "hong",
+				"phone": "010-0000-0000",
+				"type": "lg",
+				"childnode": []
+			}
+		]
+	}]
+}]
+
+function pickNameDataWithSk(arr) {
+    let result = [];
+    (function closure(arr) {
+        for (obj of arr) {
+            (function nameFinder(obj) {
+                if (obj['type'] === 'sk') result.push(obj['name'])
+                if (obj['childnode'].length !== 0) return closure(obj['childnode']);
+            })(obj);
+        }
+    })(arr);
+    return result;
+}
+// console.log(pickNameDataWithSk(jsonData));
+//type이 sk인, name으로 구성된 배열만 출력해본다.
+//["Yong", "hary", "solvin", "hani", "chulsu"]
