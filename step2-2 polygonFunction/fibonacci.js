6741cadb63f38364115e8afa458f63ef042d4196
aftersnowing <gurcks2@gmail.com>
2019-05-14 02:43:18 +0900

    tail recursion 추가

--- step2-2 polygonFunction/fibonacci.js
+++ step2-2 polygonFunction/fibonacci.js
@@ -1,12 +1,18 @@
 function fibonacci(n) {
-    var a = 1, b = 0, temp;
-    while (n > 0) {
-        temp = a;
-        a = a + b;
-        b = temp;
+    var previousFibo = 1, prepreviousFibo = 0, currentFibo;
+
+    if (n < 2) return n
+
+    while (n > 1) {
+        currentFibo = previousFibo + prepreviousFibo;
+
+        prepreviousFibo = previousFibo;
+
+        previousFibo = currentFibo;
+
         n--;
     }
-    return b
+    return currentFibo
 }
 // console.log(fibonacci(0));
 // console.log(fibonacci(1));
@@ -20,12 +26,8 @@ function fibonacci(n) {
 
 
 function fibonacciRecursion(n) {
-    if (n === 0) {
-        return 0;
-    }
-    if (n == 1) {
-        return 1;
-    }
+    if (n === 0) return 0
+    if (n == 1) return 1
     return fibonacciRecursion(n - 1) + fibonacciRecursion(n - 2);
 }
 // console.log(fibonacciRecursion(0));
@@ -43,11 +45,9 @@ function fibonacciRecursion(n) {
 function fibonacciMemoization(n) {
     let d = [0, 1];
     return (function memoArrClosure(n) {
-        if (d[n] !== undefined) {
-            return d[n];
-        }
-        d[n] = memoArrClosure(n-1) + memoArrClosure(n-2) 
-        return d[n];
+        if (d[n] !== undefined) return d[n]
+        else d[n] = memoArrClosure(n - 1) + memoArrClosure(n - 2)
+        return d[n]
     })(n);
 }
 // console.log(fibonacciMemoization(0));
@@ -62,3 +62,16 @@ function fibonacciMemoization(n) {
 // recursion을 사용한 코드에 memoization을 추가하고 closure형태로 만듬
 
 
+
+function fibonacciTailRecursion(n, previousFibo, prepreviousFibo) {
+    if (n < 1) return prepreviousFibo
+    if (n < 2) return previousFibo
+    return fibonacciTailRecursion(n - 1, previousFibo + prepreviousFibo, previousFibo)
+}
+// console.log(fibonacciTailRecursion(0, 1, 0));
+// console.log(fibonacciTailRecursion(1, 1, 0));
+// console.log(fibonacciTailRecursion(2, 1, 0));
+// console.log(fibonacciTailRecursion(3, 1, 0));
+// console.log(fibonacciTailRecursion(4, 1, 0));
+// console.log(fibonacciTailRecursion(5, 1, 0));
+// console.log(fibonacciTailRecursion(8950, 1, 0));
