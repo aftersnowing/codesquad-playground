000beccabea27849c8d34d08835b3373d083ec2f
aftersnowing <gurcks2@gmail.com>
2019-06-17 16:40:24 +0900

    fix: modify indent

--- step6-1 arrayParser - 1/arrayParser.js
+++ step6-1 arrayParser - 1/arrayParser.js
@@ -1,59 +1,56 @@
 class ArrayParser {
-    constructor() {
-    }
+  constructor() {}
 
-    tokenizer(str) {
-        const splitedStr = str.split(', ')
-            .reduce((acc, cur) => {
-                acc.push(...cur.match(/\[|\]|\d+/g))
-                return acc
-            }, []);
-        this.tokenizedData = splitedStr;
-    }
+  tokenizer(str) {
+    const splitedStr = str.split(", ").reduce((acc, cur) => {
+      acc.push(...cur.match(/\[|\]|\d+/g));
+      return acc;
+    }, []);
+    this.tokenizedData = splitedStr;
+  }
 
-    lexer(arr) {
-        let pointer = -1;
-        const stack = arr.reduce((acc, cur) => {
-            if (cur === '[') {
-                pointer++;
-                acc.push([]);
-            } else if (!isNaN(Number(cur))) {
-                acc[pointer].push(cur);
-            } else if (cur === ']' && pointer !== 0) {
-                pointer--;
-                acc[pointer].push(acc.pop());
-            }
-            return acc
-        }, []);
-        this.lexedData = stack[0];
-    }
+  lexer(arr) {
+    let pointer = -1;
+    const stack = arr.reduce((acc, cur) => {
+      if (cur === "[") {
+        pointer++;
+        acc.push([]);
+      } else if (!isNaN(Number(cur))) {
+        acc[pointer].push(cur);
+      } else if (cur === "]" && pointer !== 0) {
+        pointer--;
+        acc[pointer].push(acc.pop());
+      }
+      return acc;
+    }, []);
+    this.lexedData = stack[0];
+  }
 
-    parser(arr) {
-        const resultObj = {
-            type: 'array',
-            child: []
+  parser(arr) {
+    const resultObj = {
+      type: "array",
+      child: []
+    };
+    arr.forEach(val => {
+      if (Array.isArray(val)) {
+        resultObj.child.push(this.parser(val));
+      } else {
+        const obj = {
+          type: "number",
+          value: val,
+          child: []
         };
-        arr.forEach((val) => {
-            if (Array.isArray(val)) {
-                resultObj.child.push(this.parser(val));
-            } else {
-                const obj = {
-                    type: 'number',
-                    value: val,
-                    child: []
-                };
-                resultObj.child.push(obj);
-            }
-        });
-        return resultObj;
-    }
+        resultObj.child.push(obj);
+      }
+    });
+    return resultObj;
+  }
 }
 
 const arrayParser = new ArrayParser();
 
-
 //testCase//
-arrayParser.tokenizer("[123, 22, 33]")
+arrayParser.tokenizer("[123, 22, 33]");
 // arrayParser.tokenizer("[123, [22, 44], [11, 22, [33, 44, 55], 66], 33]");
 // arrayParser.tokenizer("[[123, 22, 33], [1, 2, 3]]");
 console.log(arrayParser.tokenizedData);
